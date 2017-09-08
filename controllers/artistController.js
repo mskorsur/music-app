db6dd30201c5f89e0b15d1df2b3b9c57ae2ea921
mskorsur <m.skorsur@gmail.com>
2017-09-08 21:11:09 +0200

    Cleanup + update implemented

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -9,12 +9,12 @@ exports.getArtistList = function(req, res, next) {
                 return next(err);
             }
 
-            let artistListFormatted = formatArtistData(artistList);
+            let artistListFormatted = formatArtistListData(artistList);
             res.json(artistListFormatted);
         });
 };
 
-function formatArtistData(artistList) {
+function formatArtistListData(artistList) {
     return artistList.map(function formatArtist(artist) {
         return {
             id: artist._id,
@@ -26,9 +26,42 @@ function formatArtistData(artistList) {
 }
 
 exports.getSingleArtist = function(req, res, next) {
-    let artistID = req.params.id;
+    let artistId = req.params.id;
+
+    Artist.findById(artistId)
+        .populate('albums')
+        .exec(function doneGettingSingleArtist(err, artist) {
+            if (err) {
+                return next(err);
+            }
+
+            artist = formatArtistData(artist);
+            res.json(artist);
+        });
 
 };
+
+function formatArtistData(artist) {
+    let artistAlbums = formatArtistAlbums(artist.albums);
+
+    return {
+        id: artist._id,
+        name: artist.name,
+        dateOfBirth: artist.getDateOfBirthFormatted,
+        albums: artistAlbums,
+        url: artist.url
+    }
+}
+
+function formatArtistAlbums(artistAlbums) {
+    return artistAlbumsFormatted = artistAlbums.map(album => {
+        return {
+            id: album._id,
+            name: album.name,
+            url: album.url
+        }
+    });
+}
 
 exports.createSingleArtist = function(req, res, next) {
     req.checkBody('name', 'Artist name required');
@@ -58,5 +91,39 @@ exports.createSingleArtist = function(req, res, next) {
 };
 
 exports.updateSingleArtist = function(req, res, next) {
+    let artistId = req.params.id;
+    req.checkBody('name', 'Artist name required');
+
+    req.sanitize('name').escape();
+    req.sanitize('name').trim();
+
+    let errors = req.validationErrors();
+    let artist = new Artist({
+        _id: artistId,
+        name: req.body.name,
+        dateOfBirth: req.body.dateOfBirth || '',
+        albums: (req.body.albums == null) ? [] : req.body.albums.split(',')//album ids need to be sent from client
+    });
+
+    if (errors) {
+        res.json({message: 'Error occured', error: errors});
+        return;
+    }
+    else {
+        Artist.findById(artistId, function doneFindingArtist(err, foundArtist) {
+            if (err) { return next(err); }
+
+           foundArtist._id = artistId;
+           foundArtist.name = artist.name;
+           foundArtist.dateOfBirth = (artist.dateOfBirth == null) ? foundArtist.dateOfBirth : artist.dateOfBirth;
+           foundArtist.albums.push(...artist.albums);
+
+           foundArtist.save(function doneSavingArtist(err, updatedArtist) {
+               if (err) { return next(err); }
+
+               res.json({message: 'Artist updated successfully', artist_data: updatedArtist});
+           });
+        });
+    }
 
 };
