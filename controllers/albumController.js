a8aacd621cbc6060b1d9b6c858b290579bd844cf
mskorsur <m.skorsur@gmail.com>
2017-09-10 20:40:35 +0200

    Cleanup + update implemented

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -76,20 +76,11 @@ function formatAlbumGenreData(albumGenres) {
 }
 
 exports.createSingleAlbum = function(req, res, next) {
-    req.checkBody('name', 'Missing name').notEmpty();
-    req.checkBody('artist', 'Missing artist').notEmpty();
-    req.checkBody('genre', 'Missing genre').notEmpty();
-    
-    req.sanitize('name').escape();
-    req.sanitize('artist').escape();
-    req.sanitize('genre').escape();
-    req.sanitize('name').trim();
-    req.sanitize('artist').trim();
-    req.sanitize('genre').trim();
+    checkIfRequiredAlbumDataIsPresent(req);
+    escapeAndTrimAlbumData(req);
 
     let errors = req.validationErrors();
 
-   
     let album = new Album({
         name: req.body.name,
         artist: req.body.artist, //artist id needs to be sent here from client
@@ -105,12 +96,82 @@ exports.createSingleAlbum = function(req, res, next) {
         album.save(function doneSavingAlbum(err) {
             if (err) { return next(err); }
 
+            //album is saved successfully so update corresponding artist's album
+            //list with this newly added album
+            updateCorrespondingArtistAlbumList(req.body.artist, album);
             res.json({message: 'Album created successfully', album_id: album._id});
         });
     }
 };
 
+function checkIfRequiredAlbumDataIsPresent(req) {
+    req.checkBody('name', 'Missing name').notEmpty();
+    req.checkBody('artist', 'Missing artist').notEmpty();
+    req.checkBody('genre', 'Missing genre').notEmpty();
+}
+
+function escapeAndTrimAlbumData(req) {
+    req.sanitize('name').escape();
+    req.sanitize('artist').escape();
+    req.sanitize('genre').escape();
+    req.sanitize('name').trim();
+    req.sanitize('artist').trim();
+    req.sanitize('genre').trim();
+}
+
+function updateCorrespondingArtistAlbumList(artistId, newAlbum) {
+    Artist.findById(artistId, function doneFindingArtist(err, foundArtist) {
+        if (err) {
+            console.log('Error during artist albums update ' + err);
+        }
+
+        foundArtist.albums.push(newAlbum);
+        foundArtist.save(function doneSavingArtist(err, updatedArtist) {
+            if (err) { console.log('Error during artist albums update ' + err); }
+
+            console.log('Artist albums update successful during new album creation ' + updatedArtist);
+        });
+    });
+
+}
 
 exports.updateSingleAlbum = function(req,res) {
+    let albumId = req.params.id;
+
+    checkIfRequiredAlbumDataIsPresent(req);
+    escapeAndTrimAlbumData(req);
+
+    let errors = req.validationErrors();
+
+    let album = new Album({
+        _id: albumId,
+        name: req.body.name,
+        artist: req.body.artist, //artist id needs to be sent here from client
+        releaseDate: req.body.releaseDate || '',
+        genre: req.body.genre.split(',') //genre ids need to be sent here from client
+    });
+
+    if (errors) {
+        res.json({message: 'Error occured', error: errors});
+        return;
+    } 
+    else {
+        Album.findById(albumId, function doneFindingAlbum(err, foundAlbum) {
+            if (err) { return next(err); }
+
+            foundAlbum._id = albumId;
+            foundAlbum.name = album.name;
+            foundAlbum.artist = album.artist;
+            foundAlbum.releaseDate = (album.releaseDate == null) ? foundAlbum.releaseDate : album.releaseDate;
+            foundAlbum.genre.push(...album.genre);
+
+            foundAlbum.save(function doneSavingAlbum(err, updatedAlbum) {
+                if (err) { return next(err); }
+
+                res.json({message: 'Album updated successfully', album_data: updatedAlbum});
+            });
+
+        });
+    }
 
 };
