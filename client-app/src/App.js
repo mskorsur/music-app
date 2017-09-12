d8c9ccbc687f72a4c41cabbc4f90fda8be986f8c
mskorsur <m.skorsur@gmail.com>
2017-09-12 20:54:16 +0200

    Update root components

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Navigation';
 import ListItemContainer from './ListItemContainer';
-import DetailsContainer from './DetailsContainer';
+import ArtistDetails from './ArtistDetails';
+import AlbumDetails from './AlbumDetails';
 
 const ARTISTS = [
   {id: 1, name: 'Lady Gaga', url: '/artist/LadyGaga'},
@@ -11,12 +12,16 @@ const ARTISTS = [
   {id: 4, name: 'The Weeknd', url: '/artist/TheWeeknd'},
 ];
 
+const ARTIST = {name: 'Lady Gaga', dob: '28-3-1986', albums: ['Joanne', 'The Fame Monster', 'Born This Way']};
+const ALBUM = {name: 'Born This Way', artist: 'Lady Gaga', releaseDate: '23-5-2011', genre: ['Pop', 'Electronic', 'Rock']};
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      contentType: 'artists'
+      contentType: 'artist',
+      data: ''
     }
   }
 
@@ -27,8 +32,8 @@ class App extends Component {
       <Navigation />
 
       <div className="row content-row">
-        <ListItemContainer type={this.state.contentType} artists={ARTISTS}/>
-        <DetailsContainer />
+        <ListItemContainer type={this.state.contentType} data={ARTISTS}/>
+        <AlbumDetails type={this.state.contentType} data={ALBUM}/>
       </div>
 
     </div>
