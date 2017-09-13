75858971b63fc496ae8ead76fa8db50b9aca89dd
mskorsur <m.skorsur@gmail.com>
2017-09-13 20:55:27 +0200

    Welcome page route and component

---
+++ client-app/src/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+              <h3>Welcome</h3>
+              <p>View info about your favorite music artists and their work!</p>
+            </div>
+        );
+    }
+}
+
+export default Welcome;
